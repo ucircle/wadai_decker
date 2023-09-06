735fb5a2efc8578820d8b2a2fe0144bff68c1bd6
ucircle <137670323+ucircle@users.noreply.github.com>
2023-09-06 06:54:04 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+config.hosts << "70de37a8ba8446c5b5b33f3fb070efb4.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
